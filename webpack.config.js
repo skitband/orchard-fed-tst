40773464d09a0d368faa19befdab16bc6eafa2cb
ghost.lio <skitband00@gmail.com>
2023-10-28 01:04:49 +0800

    update css style

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
