46609838ba38c1ca08091a5f55d88e777a7b94d2
ghost.lio <skitband00@gmail.com>
2023-10-28 16:29:08 +0800

    added fontawesome and update some styling

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,18 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'webfonts'
+            }
+          }
+        ],
+      },
     ],
   },
 };
